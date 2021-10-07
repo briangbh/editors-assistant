95f928b4bc6ad80210064f85fdf7c2b5b3ffdde3
Brian Grogan <brian.grogan@bighealth.com>
2021-10-07 12:42:40 -0600

    Define snippets for Playroom

---
+++ src/snippets/index.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    group: 'Button',
+    name:  'Standard',
+    code:  '<Button label="Click Here" />'
+  },
+  {
+    group: 'Image',
+    name:  'Standard',
+    code:  '<Image character="prof" text="your favorite Scottish professor" />'
+  },
+  {
+    group: 'Question',
+    name:  'Standard',
+    code:  '<Question text="What is your Sleep Score?" />'
+  },
+  {
+    group: 'Response',
+    name:  'Standard',
+    code:  `<Response question="sleep_score"
+	              option1="1.0"
+                      option2="3.5"
+                      option3="7.5"
+                      option4="10.0" />`
+  }
+]
+
